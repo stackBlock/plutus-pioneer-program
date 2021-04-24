f9d67f7ae597207b0b512ea2bcdddabd1a63dc08
stackblock <stackchain@gmail.com>
2021-04-24 16:13:19 -0400

    week 2 - homework1 done

--- code/week02/src/Week02/Homework1.hs
+++ code/week02/src/Week02/Homework1.hs
@@ -32,23 +32,32 @@ import           Text.Printf          (printf)
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are equal!
 mkValidator :: () -> (Bool, Bool) -> ValidatorCtx -> Bool
-mkValidator _ _ _ = True -- FIX ME!
+mkValidator () r _ = traceIfFalse "Idoit, Wrong Redemer!!" $ r == (True, True) || r == (False, False)
+    -- | r == (True, True)     = True
+    -- | r == (False, False)   = True 
+    -- | otherwise             = False
 
 data Typed
 instance Scripts.ScriptType Typed where
--- Implement the instance!
+    type instance DatumType Typed = ()
+    type instance RedeemerType Typed = (Bool, Bool)
 
 inst :: Scripts.ScriptInstance Typed
-inst = undefined -- FIX ME!
+inst = Scripts.validator @Typed
+    $$(PlutusTx.compile [|| mkValidator ||])
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @() @(Bool, Bool)
+
 
 validator :: Validator
-validator = undefined -- FIX ME!
+validator = Scripts.validatorScript inst 
 
 valHash :: Ledger.ValidatorHash
-valHash = undefined -- FIX ME!
+valHash = Scripts.validatorHash validator
 
 scrAddress :: Ledger.Address
-scrAddress = undefined -- FIX ME!
+scrAddress = ScriptAddress valHash 
 
 type GiftSchema =
     BlockchainActions
