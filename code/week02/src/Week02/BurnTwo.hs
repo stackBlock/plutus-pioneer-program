802a7abaacbb4cb1bde3500c2eff0cf4129b1fe5
stackblock <stackchain@gmail.com>
2021-04-24 14:40:05 -0400

    week 2 - worked on BurnTwo contract

--- code/week02/src/Week02/BurnTwo.hs
+++ code/week02/src/Week02/BurnTwo.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE NoImplicitPrelude   #-}
-{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE NoImplicitPrelude   #-} -- prevents normal haskell Prelude from being imported
+{-# LANGUAGE OverloadedStrings   #-} -- alows us to use string literals
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE TypeApplications    #-}
@@ -17,7 +17,7 @@ import           Data.Void           (Void)
 import           Plutus.Contract     hiding (when)
 import           PlutusTx            (Data (..))
 import qualified PlutusTx
-import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
+import           PlutusTx.Prelude    hiding (Semigroup(..), unless) --imported instead of regular Prelude
 import           Ledger              hiding (singleton)
 import           Ledger.Constraints  as Constraints
 import qualified Ledger.Scripts      as Scripts
@@ -25,12 +25,21 @@ import           Ledger.Ada          as Ada
 import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
 import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types    (KnownCurrency (..))
-import           Prelude             (Semigroup (..))
+import           Prelude             (Semigroup (..)) -- Imported one function for special reason.
 import           Text.Printf         (printf)
 
+
+    -- INLINABLE makes the fuction able to be used by Temlate Haskell via the validator function
 {-# INLINABLE mkValidator #-}
 mkValidator :: Data -> Data -> Data -> ()
-mkValidator _ _ _ = traceError "NO WAY!"
+mkValidator _ r _ 
+    | r == I 42 = ()
+    | otherwise = traceError "Wrong reedemer"
+
+
+-- {-# INLINABLE mkValidator #-}
+-- mkValidator :: Data -> Data -> Data -> ()
+-- mkValidator _ _ _ = traceError "NO WAY!"
 
 validator :: Validator
 validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
@@ -44,7 +53,7 @@ scrAddress = ScriptAddress valHash
 type GiftSchema =
     BlockchainActions
         .\/ Endpoint "give" Integer
-        .\/ Endpoint "grab" ()
+        .\/ Endpoint "grab" Integer
 
 give :: (HasBlockchainActions s, AsContractError e) => Integer -> Contract w s e ()
 give amount = do
@@ -53,14 +62,14 @@ give amount = do
     void $ awaitTxConfirmed $ txId ledgerTx
     logInfo @String $ printf "made a gift of %d lovelace" amount
 
-grab :: forall w s e. (HasBlockchainActions s, AsContractError e) => Contract w s e ()
-grab = do
+grab :: forall w s e. (HasBlockchainActions s, AsContractError e) => Integer -> Contract w s e ()
+grab r = do
     utxos <- utxoAt scrAddress
     let orefs   = fst <$> Map.toList utxos
         lookups = Constraints.unspentOutputs utxos      <>
                   Constraints.otherScript validator
         tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ I 17 | oref <- orefs]
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ I r | oref <- orefs]
     ledgerTx <- submitTxConstraintsWith @Void lookups tx
     void $ awaitTxConfirmed $ txId ledgerTx
     logInfo @String $ "collected gifts"
@@ -69,7 +78,7 @@ endpoints :: Contract () GiftSchema Text ()
 endpoints = (give' `select` grab') >> endpoints
   where
     give' = endpoint @"give" >>= give
-    grab' = endpoint @"grab" >>  grab
+    grab' = endpoint @"grab" >>=  grab
 
 mkSchemaDefinitions ''GiftSchema
 
