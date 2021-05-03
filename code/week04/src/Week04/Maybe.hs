9b3cc505969d580c854625d2af726a15e7e9859a
stackblock <stackchain@gmail.com>
2021-05-03 14:01:36 -0400

    worked on Maybe lesson

--- code/week04/src/Week04/Maybe.hs
+++ code/week04/src/Week04/Maybe.hs
@@ -1,7 +1,6 @@
 module Week04.Maybe where
 
 import Text.Read (readMaybe)
-import Week04.Monad
 
 foo :: String -> String -> String -> Maybe Int
 foo x y z = case readMaybe x of
@@ -10,17 +9,43 @@ foo x y z = case readMaybe x of
         Nothing -> Nothing
         Just l  -> case readMaybe z of
             Nothing -> Nothing
-            Just m  -> Just (k + l + m)
+            Just n  -> Just (k + l + n)
 
-bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
-bindMaybe Nothing  _ = Nothing
-bindMaybe (Just x) f = f x
+bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b 
+bindMaybe Nothing  _ = Nothing 
+bindMaybe (Just x) f = f x 
 
-foo' :: String -> String -> String -> Maybe Int
-foo' x y z = readMaybe x `bindMaybe` \k ->
-             readMaybe y `bindMaybe` \l ->
-             readMaybe z `bindMaybe` \m ->
-             Just (k + l + m)
 
-foo'' :: String -> String -> String -> Maybe Int
-foo'' x y z = threeInts (readMaybe x) (readMaybe y) (readMaybe z)
+foo' :: String -> String -> String -> Maybe Int 
+foo' x y z =    readMaybe x `bindMaybe` \k ->
+                readMaybe y `bindMaybe` \l ->
+                readMaybe z `bindMaybe` \m ->
+                Just (k + l + m)
+
+
+
+
+-- import Text.Read (readMaybe)
+-- import Week04.Monad
+
+-- foo :: String -> String -> String -> Maybe Int
+-- foo x y z = case readMaybe x of
+--     Nothing -> Nothing
+--     Just k  -> case readMaybe y of
+--         Nothing -> Nothing
+--         Just l  -> case readMaybe z of
+--             Nothing -> Nothing
+--             Just m  -> Just (k + l + m)
+
+-- bindMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+-- bindMaybe Nothing  _ = Nothing
+-- bindMaybe (Just x) f = f x
+
+-- foo' :: String -> String -> String -> Maybe Int
+-- foo' x y z = readMaybe x `bindMaybe` \k ->
+--              readMaybe y `bindMaybe` \l ->
+--              readMaybe z `bindMaybe` \m ->
+--              Just (k + l + m)
+
+-- foo'' :: String -> String -> String -> Maybe Int
+-- foo'' x y z = threeInts (readMaybe x) (readMaybe y) (readMaybe z)
