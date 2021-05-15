88a1ff382411edb2115bb2bc80b8fe68ba5cee84
stackblock <stackchain@gmail.com>
2021-05-15 13:09:34 -0400

    completed signed and free exercises

--- code/week05/src/Week05/Free.hs
+++ code/week05/src/Week05/Free.hs
@@ -72,6 +72,29 @@ mkSchemaDefinitions ''FreeSchema
 mkKnownCurrencies []
 
 
+-- comandline testing
+
+test :: IO()
+test = runEmulatorTraceIO $ do  
+    let tn = "ABC"
+    h1 <- activateContractWallet (Wallet 1) endpoints
+    h2 <- activateContractWallet (Wallet 2) endpoints   
+    callEndpoint @"mint" h1 $ MintParams
+        { mpTokenName   = tn 
+        , mpAmount      = 555
+        }
+    callEndpoint @"mint" h2 $ MintParams 
+        { mpTokenName   = tn 
+        , mpAmount      = 444
+        }
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"mint" h1 $ MintParams 
+        { mpTokenName   = tn 
+        , mpAmount      = -222
+        }
+    void $ Emulator.waitNSlots 1
+
+
 
 
 
@@ -142,22 +165,22 @@ mkKnownCurrencies []
 
 -- mkKnownCurrencies []
 
-test :: IO ()
-test = runEmulatorTraceIO $ do
-    let tn = "ABC"
-    h1 <- activateContractWallet (Wallet 1) endpoints
-    h2 <- activateContractWallet (Wallet 2) endpoints
-    callEndpoint @"mint" h1 $ MintParams
-        { mpTokenName = tn
-        , mpAmount    = 555
-        }
-    callEndpoint @"mint" h2 $ MintParams
-        { mpTokenName = tn
-        , mpAmount    = 444
-        }
-    void $ Emulator.waitNSlots 1
-    callEndpoint @"mint" h1 $ MintParams
-        { mpTokenName = tn
-        , mpAmount    = -222
-        }
-    void $ Emulator.waitNSlots 1
+-- test :: IO ()
+-- test = runEmulatorTraceIO $ do
+--     let tn = "ABC"
+--     h1 <- activateContractWallet (Wallet 1) endpoints
+--     h2 <- activateContractWallet (Wallet 2) endpoints
+--     callEndpoint @"mint" h1 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 555
+--         }
+--     callEndpoint @"mint" h2 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 444
+--         }
+--     void $ Emulator.waitNSlots 1
+--     callEndpoint @"mint" h1 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = -222
+--         }
+--     void $ Emulator.waitNSlots 1
--- code/week05/src/Week05/Signed.hs
+++ code/week05/src/Week05/Signed.hs
@@ -32,22 +32,27 @@ import           Playground.Types           (KnownCurrency (..))
 import           Text.Printf                (printf)
 import           Wallet.Emulator.Wallet
 
+
+
+
 {-# INLINABLE mkPolicy #-}
-mkPolicy :: PubKeyHash -> ScriptContext -> Bool
-mkPolicy pkh ctx = txSignedBy (scriptContextTxInfo ctx) pkh
 
-policy :: PubKeyHash -> Scripts.MonetaryPolicy
-policy pkh = mkMonetaryPolicyScript $
-    $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy . mkPolicy ||])
-    `PlutusTx.applyCode`
-    PlutusTx.liftCode pkh
+mkPolicy :: PubKeyHash -> ScriptContext -> Bool 
+mkPolicy pkh ctx = txSignedBy (scriptContextTxInfo ctx) pkh 
 
-curSymbol :: PubKeyHash -> CurrencySymbol
+policy :: PubKeyHash -> Scripts.MonetaryPolicy  
+policy pkh = mkMonetaryPolicyScript $ $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy . mkPolicy ||]) `PlutusTx.applyCode` PlutusTx.liftCode pkh
+
+curSymbol :: PubKeyHash -> CurrencySymbol 
 curSymbol = scriptCurrencySymbol . policy
+-- OR
+-- curSymbol pkh = scriptCurrencySymbol $ policy pkh 
 
+
+ 
 data MintParams = MintParams
-    { mpTokenName :: !TokenName
-    , mpAmount    :: !Integer
+    { mpTokenName :: !TokenName 
+    , mpAmount      :: !Integer
     } deriving (Generic, ToJSON, FromJSON, ToSchema)
 
 type SignedSchema =
@@ -56,22 +61,26 @@ type SignedSchema =
 
 mint :: MintParams -> Contract w SignedSchema Text ()
 mint mp = do
-    pkh <- pubKeyHash <$> Contract.ownPubKey
+    pkh <- pubKeyHash <$> Contract.ownPubKey 
+    --or
+--  pkh <- fmap pubKeyHash Contract.ownPubKey 
     let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
         lookups = Constraints.monetaryPolicy $ policy pkh
-        tx      = Constraints.mustForgeValue val
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ txId ledgerTx
+        tx      = Constraints.mustForgeValue val 
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx 
+    void $ awaitTxConfirmed $ txId ledgerTx 
     Contract.logInfo @String $ printf "forged %s" (show val)
 
 endpoints :: Contract () SignedSchema Text ()
 endpoints = mint' >> endpoints
   where
-    mint' = endpoint @"mint" >>= mint
+    mint' = endpoint @"mint" >>= mint 
 
-mkSchemaDefinitions ''SignedSchema
+mkSchemaDefinitions ''SignedSchema 
 
-mkKnownCurrencies []
+mkKnownCurrencies []    
+
+-- cli
 
 test :: IO ()
 test = runEmulatorTraceIO $ do
@@ -92,3 +101,94 @@ test = runEmulatorTraceIO $ do
         , mpAmount    = -222
         }
     void $ Emulator.waitNSlots 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- {-# INLINABLE mkPolicy #-}
+-- mkPolicy :: PubKeyHash -> ScriptContext -> Bool
+-- mkPolicy pkh ctx = txSignedBy (scriptContextTxInfo ctx) pkh
+
+-- policy :: PubKeyHash -> Scripts.MonetaryPolicy
+-- policy pkh = mkMonetaryPolicyScript $
+--     $$(PlutusTx.compile [|| Scripts.wrapMonetaryPolicy . mkPolicy ||])
+--     `PlutusTx.applyCode`
+--     PlutusTx.liftCode pkh
+
+-- curSymbol :: PubKeyHash -> CurrencySymbol
+-- curSymbol = scriptCurrencySymbol . policy
+
+-- data MintParams = MintParams
+--     { mpTokenName :: !TokenName
+--     , mpAmount    :: !Integer
+--     } deriving (Generic, ToJSON, FromJSON, ToSchema)
+
+-- type SignedSchema =
+--     BlockchainActions
+--         .\/ Endpoint "mint" MintParams
+
+-- mint :: MintParams -> Contract w SignedSchema Text ()
+-- mint mp = do
+--     pkh <- pubKeyHash <$> Contract.ownPubKey
+--     let val     = Value.singleton (curSymbol pkh) (mpTokenName mp) (mpAmount mp)
+--         lookups = Constraints.monetaryPolicy $ policy pkh
+--         tx      = Constraints.mustForgeValue val
+--     ledgerTx <- submitTxConstraintsWith @Void lookups tx
+--     void $ awaitTxConfirmed $ txId ledgerTx
+--     Contract.logInfo @String $ printf "forged %s" (show val)
+
+-- endpoints :: Contract () SignedSchema Text ()
+-- endpoints = mint' >> endpoints
+--   where
+--     mint' = endpoint @"mint" >>= mint
+
+-- mkSchemaDefinitions ''SignedSchema
+
+-- mkKnownCurrencies []
+
+-- test :: IO ()
+-- test = runEmulatorTraceIO $ do
+--     let tn = "ABC"
+--     h1 <- activateContractWallet (Wallet 1) endpoints
+--     h2 <- activateContractWallet (Wallet 2) endpoints
+--     callEndpoint @"mint" h1 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 555
+--         }
+--     callEndpoint @"mint" h2 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = 444
+--         }
+--     void $ Emulator.waitNSlots 1
+--     callEndpoint @"mint" h1 $ MintParams
+--         { mpTokenName = tn
+--         , mpAmount    = -222
+--         }
+--     void $ Emulator.waitNSlots 1
